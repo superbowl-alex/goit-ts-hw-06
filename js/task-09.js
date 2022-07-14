c60523ed854c837f21a9b5219f5c86d12bb1a771
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2022-07-14 16:30:47 +0300

    Done task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.body;
+const btnEl = document.querySelector("button");
+const spanEl = document.querySelector(".color");
+
+const changeBackgroundColor = () => {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  spanEl.textContent = randomColor;
+};
+
+btnEl.addEventListener("click", changeBackgroundColor);
